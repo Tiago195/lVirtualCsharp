8047e92c6a406306020fbb5a77343b4f3223ed8c
Tiago195 <tiagocf475@gmail.com>
2022-12-26 15:20:56 -0300

    chore: :construction: add fluent db

--- lVirtual.ProductApi/Context/AppDbContext.cs
+++ lVirtual.ProductApi/Context/AppDbContext.cs
@@ -9,4 +9,33 @@ public class AppDbContext : DbContext
 
   public DbSet<Product> Products { get; set; }
   public DbSet<Category> Categories { get; set; }
+
+
+  protected override void OnModelCreating(ModelBuilder mb)
+  {
+    // Product
+
+    mb.Entity<Product>().HasKey(p => p.Id);
+    mb.Entity<Product>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+    mb.Entity<Product>().Property(p => p.Description).HasMaxLength(255).IsRequired();
+    mb.Entity<Product>().Property(p => p.ImageURL).HasMaxLength(255).IsRequired();
+    mb.Entity<Product>().Property(p => p.Price).HasPrecision(12, 2);
+
+    mb.Entity<Product>().HasData(
+      new Product { Id = 1, Name = "Caderno", Price = 7.55M, Description = "Caderno Espiral", Stock = 10, ImageURL = "carderno1.jpg", CategoryId = 1 },
+      new Product { Id = 2, Name = "Lápis", Price = 3.45M, Description = "Lápis Preto", Stock = 20, ImageURL = "lapis1.jpg", CategoryId = 1 },
+      new Product { Id = 3, Name = "Clips", Price = 5.33M, Description = "Clipis para papel", Stock = 50, ImageURL = "clipis1.jpg", CategoryId = 2 }
+    );
+
+    // Category
+
+    mb.Entity<Category>().HasKey(c => c.CategoryId);
+    mb.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+    mb.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).IsRequired().OnDelete(DeleteBehavior.Cascade);
+
+    mb.Entity<Category>().HasData(
+      new Category { CategoryId = 1, Name = "Material Escolar" },
+      new Category { CategoryId = 2, Name = "Acessórios" }
+    );
+  }
 }
---
+++ lVirtual.ProductApi/Migrations/20221226180714_seedProducts.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lVirtual.ProductApi.Migrations
+{
+    public partial class seedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageURL", "Name", "Price", "Stock" },
+                values: new object[] { 1, 1, "Caderno Espiral", "carderno1.jpg", "Caderno", 7.55m, 10L });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageURL", "Name", "Price", "Stock" },
+                values: new object[] { 2, 1, "Lápis Preto", "lapis1.jpg", "Lápis", 3.45m, 20L });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageURL", "Name", "Price", "Stock" },
+                values: new object[] { 3, 2, "Clipis para papel", "clipis1.jpg", "Clips", 5.33m, 50L });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
